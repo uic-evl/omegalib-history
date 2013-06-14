46352b5ec0cc90f0929557c23f4f49487775e3b7
Alessandro Febretti <febret@gmail.com>
2013-06-14 16:51:41 -0500

    CHANGE: ModuleServices.removeModule now queues modules instead of removing immediately, so module removal can happen from within event handler and update functions.

--- src/omega/ModuleServices.cpp
+++ src/omega/ModuleServices.cpp
@@ -1,29 +1,38 @@
-/**************************************************************************************************
+/******************************************************************************
  * THE OMEGA LIB PROJECT
- *-------------------------------------------------------------------------------------------------
- * Copyright 2010-2013		Electronic Visualization Laboratory, University of Illinois at Chicago
+ *-----------------------------------------------------------------------------
+ * Copyright 2010-2013		Electronic Visualization Laboratory, 
+ *							University of Illinois at Chicago
  * Authors:										
  *  Alessandro Febretti		febret@gmail.com
- *-------------------------------------------------------------------------------------------------
- * Copyright (c) 2010-2013, Electronic Visualization Laboratory, University of Illinois at Chicago
+ *-----------------------------------------------------------------------------
+ * Copyright (c) 2010-2013, Electronic Visualization Laboratory,  
+ * University of Illinois at Chicago
  * All rights reserved.
- * Redistribution and use in source and binary forms, with or without modification, are permitted 
- * provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
  * 
- * Redistributions of source code must retain the above copyright notice, this list of conditions 
- * and the following disclaimer. Redistributions in binary form must reproduce the above copyright 
- * notice, this list of conditions and the following disclaimer in the documentation and/or other 
- * materials provided with the distribution. 
+ * Redistributions of source code must retain the above copyright notice, this 
+ * list of conditions and the following disclaimer. Redistributions in binary 
+ * form must reproduce the above copyright notice, this list of conditions and 
+ * the following disclaimer in the documentation and/or other materials provided 
+ * with the distribution. 
  * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES; LOSS OF 
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *************************************************************************************************/
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE  GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *-----------------------------------------------------------------------------
+ * What's in this file
+ *	Engine module spport. Engine modules are classes that can be attached to 
+ *	the engine and receive update, event and command calls.
+ ******************************************************************************/
 #include "omega/ModuleServices.h"
 
 using namespace omega;
@@ -31,23 +40,24 @@ using namespace omega;
 NameGenerator EngineModule::mysNameGenerator("Module_");
 
 List< Ref<EngineModule> > ModuleServices::mysModules;
+List< Ref<EngineModule> > ModuleServices::mysModulesToRemove;
 List< EngineModule* > ModuleServices::mysNonCoreModules;
 bool ModuleServices::mysCoreMode = true;
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EngineModule::enableSharedData() 
 { 
 	oassert(!myInitialized); 
 	mySharedDataEnabled = true; 
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 EngineModule::~EngineModule()
 {
 	ofmsg("~EngineModule %1%", %myName);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EngineModule::doInitialize(Engine* server) 
 { 
 	if(myEngine == NULL) myEngine = server; 
@@ -64,7 +74,7 @@ void EngineModule::doInitialize(Engine* server)
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void EngineModule::doDispose()
 {
 	if(myInitialized) 
@@ -75,7 +85,7 @@ void EngineModule::doDispose()
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void ModuleServices::addModule(EngineModule* module)
 { 
 	//ofmsg("ModuleServices::addModule: %1%", %module->getName());
@@ -84,19 +94,18 @@ void ModuleServices::addModule(EngineModule* module)
 	if(!mysCoreMode) mysNonCoreModules.push_back(module);
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void ModuleServices::removeModule(EngineModule* module)
 {
 	//ofmsg("ModuleServices::removeModule: %1%", %module->getName());
 	
 	if(module != NULL)
 	{
-		module->doDispose();
-		mysModules.remove(module);
+		mysModulesToRemove.push_back(module);
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void ModuleServices::update(Engine* srv, const UpdateContext& context)
 {
 	foreach(EngineModule* module, mysModules)
@@ -104,9 +113,17 @@ void ModuleServices::update(Engine* srv, const UpdateContext& context)
 		module->doInitialize(srv);
 		module->update(context);
 	}
+
+	// Remove modules
+	foreach(EngineModule* module, mysModulesToRemove)
+	{
+		module->doDispose();
+		mysModules.remove(module);
+	}
+	mysModulesToRemove.clear();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void ModuleServices::handleEvent(const Event& evt)
 {
 	for(int i = EngineModule::PriorityHigh; i >= EngineModule::PriorityLow; i--)
@@ -125,7 +142,7 @@ void ModuleServices::handleEvent(const Event& evt)
 	}
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 bool ModuleServices::handleCommand(const String& cmd)
 {
 	for(int i = EngineModule::PriorityHigh; i >= EngineModule::PriorityLow; i--)
@@ -145,7 +162,7 @@ bool ModuleServices::handleCommand(const String& cmd)
 	return false;
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 //void ModuleServices::initializeRenderer(Engine* srv, Renderer* r)
 //{
 //	foreach(EngineModule* module, mysModules)
@@ -155,7 +172,7 @@ bool ModuleServices::handleCommand(const String& cmd)
 //	}
 //}
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void ModuleServices::disposeAll()
 {
 	omsg("ModuleServices::disposeAll");
@@ -168,7 +185,7 @@ void ModuleServices::disposeAll()
 	mysNonCoreModules.clear();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 void ModuleServices::disposeNonCoreModules()
 {
 	omsg("ModuleServices::disposeNonCoreModules");
@@ -181,7 +198,7 @@ void ModuleServices::disposeNonCoreModules()
 	mysNonCoreModules.clear();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
 Vector<EngineModule*> ModuleServices::getModules()
 {
 	Vector<EngineModule*> ret;
