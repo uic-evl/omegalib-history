94e826cbada8b62d709e88ba832e942525623946
Alessandro Febretti <febret@gmail.com>
2013-06-26 21:36:09 -0500

    FIX: Material color atribute was missing OVERRIDE specifier loaded models would ignore color specified in applied material.

--- src/cyclops/Material.cpp
+++ src/cyclops/Material.cpp
@@ -81,8 +81,11 @@ void Material::setColor(const Color& diffuse, const Color& emissive)
 	if(myMaterial == NULL)
 	{
 		myMaterial = new osg::Material();
+		// Note the use of OVERRIDE here: we want to ignore all material 
+		// definitions possibly specified in a 3D object and use this one 
+		// instead. 
 		myStateSet->setAttributeAndModes(myMaterial, 
-			osg::StateAttribute::ON);
+			osg::StateAttribute::ON | osg::StateAttribute::OVERRIDE);
 	}
 	myMaterial->setDiffuse(osg::Material::FRONT_AND_BACK, COLOR_TO_OSG(diffuse));
 	myMaterial->setEmission(osg::Material::FRONT_AND_BACK, COLOR_TO_OSG(emissive));
