e021cc9cc32e55fc3605012bd336b6d906c093d4
Alessandro Febretti <febret@gmail.com>
2013-08-13 15:59:27 -0500

    FIX: VtkRenderPass was not seting projection matrix

--- src/omegaVtk/VtkRenderPass.cpp
+++ src/omegaVtk/VtkRenderPass.cpp
@@ -45,7 +45,8 @@
 using namespace omegaVtk;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-VtkRenderPass::VtkRenderPass(Renderer* client, const String& name): RenderPass(client, name)
+VtkRenderPass::VtkRenderPass(Renderer* client, const String& name): 
+	RenderPass(client, name, -10)
 {
 	resetPropQueues();
 }
@@ -125,6 +126,10 @@ void VtkRenderPass::render(Renderer* mng, const DrawContext& context)
 		//state.renderer = mng->getRenderer();
 
 		//mng->getRootNode()->draw(&state);
+
+		glMatrixMode(GL_PROJECTION);
+		glPushMatrix();
+		glLoadMatrixd(context.projection.data());
 
 		// For scene node drawing, we are not using the gl matrix stack, we are using our own transforms,
 		// stored inside the scene nodes. So, create a new, clean transform on the stack.
