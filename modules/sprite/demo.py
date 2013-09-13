fd6df0cf5c7d6a9242e139b3b788fd605ba13304
Alessandro Febretti <febret@gmail.com>
2013-09-13 10:58:09 -0500

    updated sprite module to use pixel sizes

--- modules/sprite/demo.py
+++ modules/sprite/demo.py
@@ -5,12 +5,14 @@ import sprite
 
 
 size = sprite.createSizeUniform()
+window_size = sprite.createWindowSizeUniform()
+window_size.setVector2f(Vector2(854, 480))
 
 for i in range(0, 1000):
 	x = random.random() * 10 - 5
 	y = random.random() * 10 - 5
 	z = -random.random() * 10
-	star = sprite.createSprite('star.png', size)
+	star = sprite.createSprite('star.png', size, window_size)
 	
 	# just for fun: color each star randomly.
 	r = random.random()
@@ -22,7 +24,7 @@ for i in range(0, 1000):
 
 # just for fun: animate star size
 def onUpdate(frame, time, dt):
-	sz = (sin(time) + 1) * 0.1
+	sz = (sin(time) + 1) * 64
 	size.setFloat(sz)
 	
 setUpdateFunction(onUpdate)
--- modules/sprite/sprite.py
+++ modules/sprite/sprite.py
@@ -6,6 +6,7 @@ from cyclops import *
 SPRITE_MODEL = 'sprite'
 SPRITE_PROGRAM = 'sprite'
 SPRITE_SIZE_UNIFORM = 'size'
+SPRITE_WINDOW_SIZE_UNIFORM = 'window_size'
 
 # Create a model with a single vertex (our sprite template)
 spriteModel = ModelGeometry.create(SPRITE_MODEL)
@@ -34,34 +35,35 @@ void main(void)
 spriteProgram.geometryShaderName = 'sprite-gs'
 spriteProgram.geometryShaderSource = '''
 uniform float size;
+uniform vec2 window_size;
 void main(void)
 {
-	float halfsize = size / 2;
+	float halfsize_x = size/window_size.x / 2.0;
+	float halfsize_y = size/window_size.y / 2.0;
 	
 	gl_FrontColor = gl_FrontColorIn[0];
 	
 	vec4 pos = gl_ProjectionMatrix * gl_PositionIn[0];
 	pos.xyz /= pos.w;
-	gl_Position.z = pos.z;
-
+	
 	gl_TexCoord[0].st = vec2(1.0,0.0);
-	gl_Position.xy = pos.xy + vec2(halfsize, -halfsize);
+	gl_Position.xyzw = vec4(pos.xy + vec2(halfsize_x, -halfsize_y), pos.z, 1.0);
 	EmitVertex();
-
+	
 	gl_TexCoord[0].st = vec2(1.0,1.0);
-	gl_Position.xy = pos.xy + vec2(halfsize, halfsize);
+	gl_Position.xyzw = vec4(pos.xy + vec2(halfsize_x, halfsize_y), pos.z, 1.0);
 	EmitVertex();
-
+	
 	gl_TexCoord[0].st = vec2(0.0,0.0);
-	gl_Position.xy = pos.xy + vec2(-halfsize, -halfsize);
+	gl_Position.xyzw = vec4(pos.xy + vec2(-halfsize_x, -halfsize_y), pos.z, 1.0);
 	EmitVertex();
-
+	
 	gl_TexCoord[0].st = vec2(0.0,1.0);
-	gl_Position.xy = pos.xy + vec2(-halfsize, halfsize);
+	gl_Position.xyzw = vec4(pos.xy + vec2(-halfsize_x, halfsize_y), pos.z, 1.0);
 	EmitVertex();
-
+	
 	EndPrimitive();
-}
+	}
 '''
 
 # Fragment shader: draw the quad using the diffuse texture.
@@ -70,7 +72,7 @@ spriteProgram.fragmentShaderSource = '''
 uniform sampler2D unif_DiffuseMap;
 void main (void)
 {
-    gl_FragColor = texture2D(unif_DiffuseMap, gl_TexCoord[0]) * gl_Color;
+    gl_FragColor = texture2D(unif_DiffuseMap, gl_TexCoord[0].st) * gl_Color;
 }
 '''
 
@@ -86,16 +88,25 @@ getSceneManager().addProgram(spriteProgram)
 def createSizeUniform():
 	size = Uniform.create(SPRITE_SIZE_UNIFORM, UniformType.Float, 1)
 	# set some decent default value.
-	size.setFloat(0.05)
+	size.setFloat(32)
 	return size
+
+# convenience function for creating the window size uniform
+def createWindowSizeUniform():
+	window_size = Uniform.create(SPRITE_WINDOW_SIZE_UNIFORM, UniformType.Vector2f, 1)
+	# set some decent default value.
+	window_size.setVector2f(Vector2(800, 600))
+	return window_size
 	
 # convenience function for creating sprites
-def createSprite(textureName, sizeUniform):
+def createSprite(textureName, sizeUniform, windowSizeUniform, depthFlag):
 	sprite = StaticObject.create(SPRITE_MODEL)
 	sprite.getMaterial().setProgram(SPRITE_PROGRAM)
 	sprite.getMaterial().setTransparent(True)
 	sprite.getMaterial().setDiffuseTexture(textureName)
+	sprite.getMaterial().setDepthTestEnabled(depthFlag)
 	sprite.getMaterial().attachUniform(sizeUniform)
+	sprite.getMaterial().attachUniform(windowSizeUniform)
 	return sprite
 
 
